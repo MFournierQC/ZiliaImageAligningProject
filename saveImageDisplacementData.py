ab808aff5cce74ca843f02af8c194fd47b1eedb8
Daniel <dccote@cervo.ulaval.ca>
2021-07-27 10:48:02 -0400

    Calculations of various properties for all database files (ONH)

---
+++ saveImageDisplacementData.py
@@ -0,0 +1,54 @@
+from processImages import *
+from zilia import *
+import csv
+from os import path
+
+def computeDisplacementForAllImages(dirPath, removeBadIm=True):
+    """
+    The goal is to save a csv file with the following columns:
+    filePath, properties, values
+    The properties will be computed for each file, and will be the following:
+    rawRosaX, rawRosaY, onhXShift, onhYShift
+    """
+    grayImages = loadImages(dirPath)
+    dataDictionary = seperateImages(grayImages, dirPath)
+    if removeBadIm:
+        dataDictionary = removeBadImages(dataDictionary)
+    rawRosaXs = dataDictionary["xCenter"]
+    rawRosaYs = dataDictionary["yCenter"]
+    grayImages = dataDictionary["image"]
+    imageNumbers = dataDictionary["imageNumber"]
+    shiftParams = findImageShift(grayImages)
+    onhXShifts, onhYShifts = shiftParams[:,1], shiftParams[:,0]
+    eyeImagePaths = getPathsFromImageNumbers(dirPath, imageNumbers)
+    return rawRosaXs, rawRosaYs, onhXShifts, onhYShifts, eyeImagePaths
+
+def saveImageData(data, fileName="displacementData"):
+    rawRosaXs, rawRosaYs, onhXShifts, onhYShifts, eyeImagePaths = data
+    if path.exists(fileName+".csv"):
+        # the file already exists
+        fileMode = 'a'
+    else:
+        # the file must first be created
+        fileMode = 'w'
+    with open(fileName+'.csv', fileMode, newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for i in range(len(eyeImagePaths)):
+            writer.writerow([eyeImagePaths[i], 'rawRosaX', int(rawRosaXs[i])])
+            writer.writerow([eyeImagePaths[i], 'rawRosaY', int(rawRosaYs[i])])
+            writer.writerow([eyeImagePaths[i], 'onhXShift', int(onhXShifts[i])])
+            writer.writerow([eyeImagePaths[i], 'onhYShift', int(onhYShifts[i])])
+
+def getPathsFromImageNumbers(dirPath, imageNumbers):
+    paths = []
+    for i in imageNumbers:
+        if len(str(i)) == 1:
+            path = dirPath+"/00"+str(i)+"-eye.jpg"
+            paths.append(path.replace("\\", "/"))
+        if len(str(i)) == 2:
+            path = dirPath+"/0"+str(i)+"-eye.jpg"
+            paths.append(path.replace("\\", "/"))
+        if len(str(i)) == 3:
+            path = dirPath+"/"+str(i)+"-eye.jpg"
+            paths.append(path.replace("\\", "/"))
+    return paths
---
+++ tests/testsComputeDisplacementForAllImages.py
@@ -0,0 +1,36 @@
+import envtest
+from processImages import *
+from saveImageDisplacementData import *
+from zilia import *
+
+dir1 = r"Z:\labdata\dcclab\zilia\March2021\Baseline 3\Bresil 1511184\20210316-095955-bresil-od-onh-rlp2"
+dir2 = r"Z:\labdata\dcclab\zilia\March2021\Baseline 3\Bresil 1511184\20210316-100153-bresil-od-onh-rlp6"
+
+class TestComputeDisplacementForAllImages(envtest.ZiliaTestCase):
+
+    def testInit(self):
+        self.assertTrue(True)
+
+    @envtest.skip("Works fine, but skip file creation please.")
+    def testSave2Folders(self):
+        dir1 = r".\TestImages\miniTestSampleNewData"
+        dir2 = r".\TestImages\testAlignment"
+        dirPaths = [dir1, dir2]
+        for i in range(len(dirPaths)):
+            data = computeDisplacementForAllImages(dirPaths[i])
+            saveImageData(data, fileName="displacementDataTest")
+
+    @envtest.skip("Works fine, but skip file creation please.")
+    def testComputeDisplacementFor2OnhFolders(self):
+        dataPath = "Z:/labdata/dcclab/zilia"
+        sys.path.insert(0, dataPath)
+
+        df = pd.read_csv("onhpaths.csv")
+        paths = df["path"].to_list()
+        dirPaths = paths[:2]
+        for i in range(len(dirPaths)):
+            data = computeDisplacementForAllImages(dirPaths[i])
+            saveImageData(data, fileName="displacementDataTestOn2Folders")
+
+if __name__=="__main__":
+    envtest.main()
