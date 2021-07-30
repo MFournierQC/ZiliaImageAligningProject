e1b615393bc4315ac7daf3232f5c91684afe5c36
Daniel <dccote@cervo.ulaval.ca>
2021-07-30 16:57:25 -0400

    Create testsLongCalculations.py
    New tests for long calculations on server

---
+++ tests/testsLongCalculations.py
@@ -0,0 +1,53 @@
+import env
+from dcclab.database import *
+from datetime import date
+from zilia import *
+import unittest
+import os
+import numpy as np
+from skimage.io import imread
+import subprocess
+import re
+from analyzeEyeImages import *
+
+
+def computeONHParams(grayImage):
+    detector = ZiliaONHDetector(grayImage)
+    detector.getParamsCorrections()
+    detector.preProcessImage()
+    bestEllipse = detector.findOpticNerveHead()
+    (xCenter, yCenter), minorAxis, majorAxis, orientation = bestEllipse
+    ellipseDict = { "onhCenterX": xCenter,
+                    "onhCenterY": yCenter,
+                    "onhHeight": majorAxis,
+                    "onhWidth": minorAxis}
+    return ellipseDict
+
+
+class TestZilia(env.DCCLabTestCase):
+
+    def setUp(self):
+        self.db = ZiliaDB()
+        self.assertIsNotNone(self.db)
+
+    def testGetGrayscaleEyeImagesFromDatabase(self):
+        images = self.db.getGrayscaleEyeImages(monkey='Bresil' , rlp=6, timeline='baseline 3', region='onh', limit=10)
+        self.assertTrue(len(images) == 10)
+        for image in images:
+            self.assertEqual(image.shape, (1024, 1216))
+
+
+    def testGetGrayscaleEyeImagesWithPaths(self):
+        images = self.db.getGrayscaleEyeImagesWithPaths(monkey='Bresil' , rlp=6, timeline='baseline 3', region='onh', limit=10)
+        self.assertTrue(len(images.keys()) == 10)
+        for path, imageData in images.items():
+            self.assertEqual(imageData.shape, (1024, 1216))
+            try:
+                results = computeONHParams(imageData)
+                results["path"] = path
+                self.assertIsNotNone(results)
+            except:
+                pass
+
+if __name__ == '__main__':
+    unittest.main()
