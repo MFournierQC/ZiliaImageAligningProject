14f9c1c33569d96388c46d2a8101a3ffaaffaa2f
Daniel <dccote@cervo.ulaval.ca>
2021-08-05 18:48:03 -0400

    Added setUp so db is an instance variable.

--- tests/testsImageProcessingForDatabase.py
+++ tests/testsImageProcessingForDatabase.py
@@ -1,41 +1,45 @@
 import envtest
 from processImagesFromDatabase import *
 from zilia import *
-db = ZiliaDB()
 
 # skipPlots = True
 
 class TestImageProcessingForDatabase(envtest.ZiliaTestCase):
 
+    def setUp(self):
+        super().setUp()
+        self.db = ZiliaDB()
+        self.assertIsNotNone(self.db)
+
     def testInit(self):
         self.assertTrue(True)
 
     def testImportRosaImages(self):
-        rosaImages = db.getRGBImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh'
+        rosaImages = self.db.getRGBImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh'
                                      ,content='rosa',eye='os', limit=10)
         self.assertIsNotNone(rosaImages)
         
     def testGetRosaProperties(self):
-        rosaImages = db.getRGBImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh'
+        rosaImages = self.db.getRGBImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh'
                                      , content='rosa', eye='os', limit=10)
         rosaProperties = getRosaProperties(rosaImages)
         self.assertIsNotNone(len(rosaProperties))
         self.assertTrue(len(rosaProperties) == len(rosaImages))
 
     def testImportRetinaImages(self):
-        retinaImages = db.getGrayscaleEyeImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh'
+        retinaImages = self.db.getGrayscaleEyeImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh'
                                                 , eye='os', limit=10)
         self.assertIsNotNone(retinaImages)
 
     def testFindBlurryImages(self):
-        retinaImages = db.getGrayscaleEyeImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh'
+        retinaImages = self.db.getGrayscaleEyeImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh'
                                                 , eye='os', limit=10)
         isBluryFlag =  findBlurryImages(retinaImages)
         self.assertIsNotNone(isBluryFlag)
         self.assertTrue(len(isBluryFlag) == len(retinaImages))
 
     def testCropImageMargins(self):
-        retinaImages = db.getGrayscaleEyeImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh'
+        retinaImages = self.db.getGrayscaleEyeImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh'
                                                 , eye='os', limit=3)
         margin = 100
         croppedImage = cropImageMargins(retinaImages[0], margin=margin)
