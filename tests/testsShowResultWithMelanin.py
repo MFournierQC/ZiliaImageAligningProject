7bb1ea9aef59c5ea363e4c8650999c396a0c42bc
Emile Marcotte <emilemarcotte@outlook.com>
2021-08-12 07:20:44 -0400

    Tests to get melanin values from spectra

--- tests/testsShowResultWithMelanin.py
+++ tests/testsShowResultWithMelanin.py
@@ -18,5 +18,26 @@ class TestShowResultWithMelanin(envtest.TestCase):
     def testGetMelaninCoeffs(self):
         pass
 
+    def testGetMainAnalysisOnRetinaImages(self):
+        eye='os'
+        retinaImages = self.db.getGrayscaleEyeImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh', eye=eye , limit= 10)
+        rosaImages = slef.db.getRGBImages(monkey='Bresil', rlp=6, timeline='baseline 3', region='onh', content='rosa', eye=eye , limit = 10)
+
+        # dark = findDarkImages(retinaImages)
+
+        rosaAbsoluteXY=getRosaProperties(rosaImages)
+        # useful info:  int(['center']['x']) , int(['center']['y']) , ['rradius'] , and ['found']
+
+        shiftValueFromReferenceImage , imageIsValid = calculateValidShiftsInOneAcquisition(retinaImages)
+
+        rosaLocationOnRefImage = applyShiftOnRosaCenter(rosaAbsoluteXY,shiftValueFromReferenceImage)
+
+        print(imageIsValid)
+        refImage = findRefImage(imageIsValid , retinaImages)
+
+        xONH,yONH,length = findOHNParamsInRefImage(refImage)
+
+        absoluteRosaValue = calculateRosaDistanceFromOnhInRefImage (xONH, yONH , rosaLocationOnRefImage)
+
 if __name__ == "__main__":
     envtest.main()
